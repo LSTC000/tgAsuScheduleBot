d82e5021e0ea964ec605faedebcfec0b50d33629
Ivan <razgonyaev_74@mail.ru>
2023-04-14 00:34:39 +0700

    Немного изменил комментарии.

--- data/memory_storage/memory_storage_keys.py
+++ data/memory_storage/memory_storage_keys.py
@@ -1,4 +1,4 @@
-# SCHEDULE
+# SCHEDULE.
 TARGET_KEY = 'target'
 TARGET_URL_KEY = 'target_url'
 ALLEGED_TARGET_NAME_KEY = 'alleged_target_name'
@@ -8,11 +8,11 @@ TODAY_TARGET_DATE_QUERY_URL_CODE_KEY = 'today_target_date_query_url_code'
 TOMORROW_TARGET_DATE_QUERY_URL_CODE_KEY = 'tomorrow_target_date_query_url_code'
 SCHEDULE_INLINE_KEYBOARDS_KEY = 'schedule_inline_keyboards'
 
-# INLINE CALENDAR
+# INLINE CALENDAR.
 INLINE_CALENDAR_KEY = 'inline_calendar'
 INLINE_CALENDAR_DATA_KEY = 'inline_calendar_data'
 
-# CHAT GPT
+# CHAT GPT.
 CHAT_GPT_MESSAGES_KEY = 'chat_gpt_messages'
 COUNT_CHAT_GPT_MESSAGES_KEY = 'count_chat_gpt_messages'
 LAST_CHAT_GPT_INLINE_KEYBOARD_KEY = 'last_chat_gpt_inline_keyboard'
