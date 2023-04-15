ac0c702ccb17b1cfe92385ff6f8b0e8526eee9d0
Ivan <razgonyaev_74@mail.ru>
2023-04-15 20:57:19 +0700

    Изменил сообщение команды /help.

--- handlers/users/commands/help.py
+++ handlers/users/commands/help.py
@@ -1,4 +1,4 @@
-from data.config import RATE_LIMIT_DICT, COMMAND_KEY
+from data.config import RATE_LIMIT_DICT, COMMAND_KEY, MAX_CHAT_GPT_MESSAGES, MAX_CONTENT_TOKENS
 
 from data.messages import HELP_COMMAND_MESSAGE
 
@@ -12,4 +12,7 @@ from aiogram import types
 @dp.message_handler(commands=['help'], state='*')
 @rate_limit(limit=RATE_LIMIT_DICT[COMMAND_KEY], key=COMMAND_KEY)
 async def help_command(message: types.Message) -> None:
-    await bot.send_message(chat_id=message.from_user.id, text=HELP_COMMAND_MESSAGE.format(message.from_user.first_name))
+    await bot.send_message(
+        chat_id=message.from_user.id,
+        text=HELP_COMMAND_MESSAGE.format(message.from_user.first_name, MAX_CHAT_GPT_MESSAGES, MAX_CONTENT_TOKENS)
+    )
